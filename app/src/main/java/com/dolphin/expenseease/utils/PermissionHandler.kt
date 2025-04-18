6d30d15411dbd4ebefc9d956f31adc67314dae93
Android Being <hitechbeing@gmail.com>
2025-04-18 15:38:34 +0530

    Add Reminders feature with Alarms, Setting updating or cancelling

---
+++ app/src/main/java/com/dolphin/expenseease/utils/PermissionHandler.kt
@@ -0,0 +1,27 @@
+package com.dolphin.expenseease.utils
+
+import android.app.AlarmManager
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+
+object PermissionHandler {
+
+    fun hasExactAlarmPermission(context: Context): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            alarmManager.canScheduleExactAlarms()
+        } else {
+            true // Permission not required for Android versions below 12
+        }
+    }
+
+    fun openAppSettings(context: Context) {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+            data = Uri.fromParts("package", context.packageName, null)
+        }
+        context.startActivity(intent)
+    }
+}
